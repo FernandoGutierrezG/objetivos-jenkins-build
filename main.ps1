535d7949975f85eb6708a768a33f5d6eff9766d4
Jesus Fernando Gutierrez García <jesusf.gutierrez@softtekclients.com>
2020-05-29 12:27:32 -0700

    adding main.ps1

---
+++ main.ps1
@@ -0,0 +1,20 @@
+<#
+    Name                : main.ps1
+    Creator             : Fernando Gutierrez
+    Description         : Script setup the roles and features on the windows server
+    Language            : PowerShell scripting language
+    Version             : 5.1.17134.858
+    Last Modification   : May 18 2020
+#>
+
+# variables
+Set-Variable bucketS3 -option ReadOnly -value "a204278-asg"
+
+
+
+
+
+
+###--------Main---------------
+Clear-Host
+Write-Host "- Jenkins Weekly"
